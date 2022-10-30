ae718e030b8dfb7b08452fce747c08a36f70ba4d
ahmetyanik <ahmetyank4242@gmail.com>
2022-10-30 15:01:38 +0100

    Havadurumu is created

---
+++ havadurumu/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import PrimarySearchAppBar from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <PrimarySearchAppBar/>
+    </div>
+  );
+}
+
+export default App;
